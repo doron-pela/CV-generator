1b304df9d04ed43d9785e545faa9f47a83795aa9
Doron Pela <devronpela@gmail.com>
2025-06-29 16:45:33 +0000

    Left notes for implementing Stateful Education array

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import AddEducationButton from './AddButton.jsx' 
+import AddButton from './AddButton.jsx' 
 import EducationFormSection from './EducationFormSection.jsx'
 export default function Education(props){
     const [isClicked, setClick] = useState(false);
@@ -7,8 +7,17 @@ export default function Education(props){
     return(
         <div className='education'>
             <h1>Education</h1>
-            <AddEducationButton formStatus={isClicked} onClick = {()=>setClick(!isClicked)}/>
+            <AddButton formStatus={isClicked} onClick = {()=>setClick(!isClicked)}/>
+
+            {/* Rendering needs to occur for every education object in the state's education list belonging to app.jsx */}
             {isClicked? <EducationFormSection {...props}/> : null}
+            {isClicked? <EducationFormSection {...props}/> : null} 
+
+            {/* Create add Education form button which should add another education object (empty by default) to the education list in the app.jsx. Then the form objects in the list
+                                                                                                                                                    are basically passed down as props to the corresponding 
+                                                                                                                                                    Education Form section, while also sending back the 
+                                                                                                                                                    new Education object back to the right position in the 
+                                                                                                                                                    state array on every change. */}
         </div>
     )
 }
